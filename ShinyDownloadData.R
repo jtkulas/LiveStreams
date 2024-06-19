217da4ec968894f594178dd1d6a946ad58174345
John Kulas <jtkulas@ergreports.com>
2024-06-19 13:01:53 -0500

    first draft

--- ShinyDownloadData.R
+++ ShinyDownloadData.R
@@ -1,68 +1,25 @@
 library(shiny)
-library(DT)
+library(rmarkdown)
 library(ggplot2)
 
-# Sample dataset
 data <- data.frame(
   ID = 1:10,
   Name = c("John", "Paul", "George", "Ringo", "Mick", "Keith", "Charlie", "Ronnie", "Roger", "Pete"),
   Age = c(40, 42, 38, 41, 43, 45, 39, 40, 44, 42)
 )
 
-# Define UI
-ui <- fluidPage(
-  titlePanel("Download CSV Example"),
-  fluidRow(
-    sidebarPanel(
-      sliderInput('ageRange', 'Age Range', 
-                  min = 30, 
-                  max = 45, 
-                  value = c(30, 45)),
-      plotOutput("distPlot"),
-      downloadButton("downloadPlot", "Download Histogram")
-    ),
-    mainPanel(
-      DTOutput("table"),
-      downloadButton("downloadData", "Download CSV"),
-      downloadButton("downloadReport", "Download Report")
-      )
-  )
-)
-
-# Define server logic
 server <- function(input, output) {
-  # Reactive expression to filter data based on age range
   filtered_data <- reactive({
     data[data$Age >= input$ageRange[1] & data$Age <= input$ageRange[2], ]
   })
-  
-  # Display the table
-  output$table <- renderDT({
-    filtered_data()
+  output$distPlot <- renderPlot({
+    # generate bins based on input$bins from ui.R
+    #    x    <- filtered_data$Age
+    #    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    ggplot(filtered_data(),aes(x=Age)) + geom_histogram()
   })
-  
-  # Download handlers
-  output$downloadData <- downloadHandler(
-    filename = function() {
-      paste("data-", Sys.Date(), ".csv", sep = "")
-    },
-    content = function(file) {
-      write.csv(filtered_data(), file, row.names = FALSE)
-    }
-  )
-
-## Copied from above (that works) - want to download image; 5/28/24
-  output$downloadPlot <- downloadHandler(          
-    filename = function() {
-      paste("data-", Sys.Date(), ".png", sep = "")
-    },
-    content = function(file) {
-      png(file)
-      print(ggplot(filtered_data(),aes(x=Age)) + geom_histogram()
-)
-      dev.off()
-    }
-  )
   
   output$downloadReport <- downloadHandler(
     filename = function() {
@@ -73,14 +30,15 @@ server <- function(input, output) {
     content = function(file) {
       src <- normalizePath('child_script.Rmd')
       
+
       # temporarily switch to the temp dir, in case you do not have write
       # permission to the current working directory
       owd <- setwd(tempdir())
       on.exit(setwd(owd))
       file.copy(src, 'child_script.Rmd', overwrite = TRUE)
       
-      out <- rmarkdown::render('child_script.Rmd',
-                               params = list(text = input$text,outp=input$Try),
+     out <- rmarkdown::render('child_script.Rmd',
+                               params = list(text = input$text, outp=input$Try, age=input$ageRange),
                                switch(input$format,
                                       PDF = pdf_document(), 
                                       HTML = html_document(), 
@@ -89,17 +47,22 @@ server <- function(input, output) {
       file.rename(out, file)
     }
   )
-  
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-#    x    <- filtered_data$Age
-#    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    ggplot(filtered_data(),aes(x=Age)) + geom_histogram()
-  })
 }
 
-# Run the app
-shinyApp(ui = ui, server = server)
+ui <- fluidPage(
+  tags$textarea(id="text", rows=10, cols=15, 
+                placeholder="Some placeholder text"),
+  
+  flowLayout(sliderInput('ageRange', 'Age Range', 
+                         min = 30, 
+                         max = 45, 
+                         value = c(30, 45)),
+             plotOutput("distPlot"),
+             radioButtons('format', 'Document format', c('HTML', 'Word','PDF'),
+                          inline = FALSE),
+             checkboxGroupInput("Try","Let's hope this works",choiceNames = list("include hi","include hey","include hello","include how are you"),choiceValues = list("HI","HEY","HELLO","HOW ARE YOU")),
+             downloadButton('downloadReport'))
+  
+)
 
+shinyApp(ui = ui, server = server)
